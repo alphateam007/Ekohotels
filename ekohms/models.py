ad9d2a64b256158d4f9e1409b600ead5748ca023
Ladeologun <ologunomolade21@gmail.com>
2021-05-16 17:43:06 +0100

    added models

--- ekohms/models.py
+++ ekohms/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields.reverse_related import OneToOneRel
 
-# Create your models here.
+
+class PaymentType(models.Model):
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=50)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Payment(models.Model):
+    id = models.UUIDField(primary_key=True)
+    payment_type_id = models.ForeignKey(PaymentType, on_delete=models.CASCADE)
+    customer_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    staff_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.CharField(max_length=200)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.amount
+
