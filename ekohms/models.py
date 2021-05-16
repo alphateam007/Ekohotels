4fc1106d2e89d7ec976d2265a7ec1ecd1b58d6fd
mac <mac@macs-MacBook-Air-4.local>
2021-05-16 17:07:18 +0100

    upadete: models implementation of Booking class and initial set up of other classes

--- ekohms/models.py
+++ ekohms/models.py
@@ -20,17 +20,17 @@ class RoomStatus(models.Model):
 class RoomType(models.Model):
     pass
 
-class Booking(models.Model):
-    id = models.UUIDField(primary_key=True)
-    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
-    customer_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    staff_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    payment_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField('date created')
-    updated_at = models.DateTimeField('date updated')
-
 class Payment(models.Model):
     pass
 
 class PaymentType(models.Model):
     pass
+
+class Booking(models.Model):
+    id = models.UUIDField(primary_key=True)
+    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+    customer_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    staff_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    payment_id = models.ForeignKey(Payment, on_delete=models.CASCADE)
+    created_at = models.DateTimeField('date created')
+    updated_at = models.DateTimeField('date updated')
